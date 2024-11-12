39060d13cc6327c3067b54dc5b2b5d8b4f102620
elianesilvagestao <elianesilva.gestao@gmail.com>
2024-11-12 20:50:30 -0300

    :tada: finish project

---
+++ README.md
@@ -0,0 +1,12 @@
+### Ric Londo Artist Tatoo
+
+![preview](./assets/readme2.png)
+
+Esse site foi criado em homenagem ao meu amigo Paulo Ricardo, Tatuador profissional à 8 anos.
+Utilzado o código HTML cria um site para o Estúdio de Tatuagem do artista "Ric Londono".
+A estrutura básica inclui um cabeçalho de navegação, uma seção de boas-vindas, uma galeria de tatuagens e um rodapé com informações de direitos autorais.
+Inclui o Bootstrap para um design responsivo e estilizado.
+Contém uma barra de navegação com links para "Home", "Portfólio" e "Contato".
+Exibe uma imagem de destaque com um texto de boas-vindas para os visitantes.
+Cada item de portfólio (como "Anubis", "Van Gogh", e personagens da Disney e Marvel) é exibido em um cartão com imagem.
+Inclui botões "Entre em contato" que levam para uma página de contato, permitindo a possibilidade de enviar uma mensagem pelo WhatsApp. Tem também uma imagem do logo do Instagram e cliando nele é direcionado para o perfil de Ric Londono.
---
+++ index.php
@@ -0,0 +1,311 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ric Londono / Tattoo Artis</title>
+    <!-- Adicione os links para os arquivos CSS do Bootstrap -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <link rel="icon" href="img/riclondono.png" class="rounded-circle" type="riclondono/png">
+</head>
+
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-warning">
+        <img src="img/riclondono2.png" alt="Tattoo Artis" style="width:55px;height:55px; margin-right: 25px;"><a class="navbar-brand" href="#">Ric Londono - Tattoo Artist</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#menu">Portifólio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="contato.php">Contato</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+
+    <section id="hero" class="d-flex align-items-center bg-dark text-white">
+        <div class="container text-center">
+    
+    <style>
+    .corner-image {
+        position: none;
+      top: 20px;
+      right: 20px;
+      width: 240px;
+      height:240px;
+      float: right;
+    }
+  </style>
+            <br><br><img src="img/riclondono2.png" class="corner-image rounded-circle">
+            <h1><b>Bem-vindo ao Stúdio de Tatuagem Ric Londono</b></h1>
+            <h4><p><p><p>Descubra a tatuagem que vai além da estética e que conte uma história única.<p></p></p><h4>
+            <p>Ric Londono é o artista perfeito para dar vida às suas ideias.</p><br>
+            
+        </div>
+    </section>
+
+    <section id="menu" class="py-5 bg-dark text-black">
+        <div class="container">
+            <h1 class="text-center mb-4 text-white">Portifólio Ric Londono</h1>
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="card mb-4">
+                        <img src="img/anubis.png" class="card-img-top" alt="Anubis">
+                        <div class="card-body">
+                            <h5 class="card-title">Anubis - Mitologia</h5>
+                            <p class="card-text">Anúbis é uma divindade da mitologia egípcia, associado à morte, ao além-túmulo, ao embalsamento e às almas perdidas.
+                            Uma tatuagem de Anúbis pode ter vários significados. Ex.: Proteção; Eternidade; Orientação Espiritual e Sabedoria.</p>
+                            <p class="card-number">A partir de R$ 399,99</p>
+                            <a href="contato.php" class="btn btn-primary add-to-cart"data-nome="Item 1" data-preco="10">Entre em contato <target="_blank"></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="card mb-4">
+                        <img src="img/vangogh.png" class="card-img-top" alt="Noite estrelada - Van Gog">
+                        <div class="card-body">
+                            <h5 class="card-title">Noite estrelada - Van Gog</h5>
+                            <p class="card-text">Van Gogh pintou “A Noite Estrelada” durante sua estadia no asilo de Saint-Paul-de-Mausole em 1889.
+                             O significado da tatuagem de A Noite Estrelada de Van Gogh pode ser multifacetado e aberto a interpretações.</p>
+                            <p class="card-number">A partir de R$ 249,99</p>
+                            <a href="contato.php" class="btn btn-primary add-to-cart"data-nome="Item 1" data-preco="10">Entre em contato <target="_blank"></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="card mb-4">
+                        <img src="img/antonioencanto.png" class="card-img-top" alt="Antônio - do filme Encanto">
+                        <div class="card-body">
+                            <h5 class="card-title">Mundo Disney - Antônio do filme "Encanto"</h5>
+                            <p class="card-text">Antônio Madrigal é um personagem importante no filme de animação de 2021 da Disney, Encanto.
+                            Ele é o filho caçula de Pepa e Félix Madrigal , e o irmão caçula de Dolores e Camilo. Essa tatto foi uma homenagem ao neto do Don.</p>
+                            <p class="card-number">A partir de R$ 399,99</p>
+                            <a href="contato.php" class="btn btn-primary add-to-cart"data-nome="Item 1" data-preco="10">Entre em contato <target="_blank"></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="card mb-4">
+                        <img src="img/mufasa.png" class="card-img-top" alt="Mufasa do filme o Rei Leão">
+                        <div class="card-body">
+                            <h5 class="card-title">Mundo Disney - Mufasa do filme "O Rei Leão"</h5>
+                            <p class="card-text">Mufasa é um personagem importante no filme de animação de 1994 da Disney, O Rei Leão.
+                            A Tatuagem de Mufasa, pode ter vários significados, como: Monarquia; Liderança, .</p>
+                            <p class="card-number">A partir de R$ 399,99</p>
+                            <a href="contato.php" class="btn btn-primary add-to-cart"data-nome="Item 1" data-preco="10">Entre em contato <target="_blank"></a>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="card mb-4">
+                        <img src="img/dulcemaria.png" class="card-img-top" alt="Dulce Maria do RBD">
+                        <div class="card-body">
+                            <h5 class="card-title">Universo RBD - Dulce Maria</h5>
+                            <p class="card-text">Dulce María Espinosa Saviñón é uma cantora, atriz, escritora, compositora, modelo e apresentadora mexicana.
+                            Também desenvolve trabalhos paralelos na área da literatura e da filantropia.
+                            Tornou-se conhecida com o personagem Roberta Pardo na novela Rebelde.</p>
+                            <p class="card-number">A partir de R$ 599,99</p>
+                            <a href="contato.php" class="btn btn-primary add-to-cart"data-nome="Item 1" data-preco="10">Entre em contato <target="_blank"></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="card mb-4">
+                        <img src="img/rbd2.png" class="card-img-top" alt="RBD - No pares nunca de soñar">
+                        <div class="card-body">
+                            <h5 class="card-title">Universo RBD - "No pares nunca de soñar"</h5>
+                            <p class="card-text">“No pares nunca de sõnar” é um trecho de uma das músicas da banda mexicana RBD que fala sobre nunca desistir dos sonhos.
+                            Data de lançamento: 2006. Álbum: Live in Hollywood. O RBD, conhecido por suas canções que abordam temas como amor, amizade e liberdade, utiliza 'No Pares' para transmitir uma mensagem de empoderamento.
+                            A canção encoraja a quebrar barreiras, enfrentar o medo e viver a vida com intensidade e amor.
+                            Tatoo eternizado por um fã do RBD e amigo pessoal do artista tatuador Ric Londono</p>
+                            <p class="card-number">A partir de R$ 299,99</p>
+                            <a href="contato.php" class="btn btn-primary add-to-cart"data-nome="Item 1" data-preco="10">Entre em contato <target="_blank"></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="card mb-4">
+                        <img src="img/spiderman.png" class="card-img-top" alt="Homem Aranha">
+                        <div class="card-body">
+                            <h5 class="card-title">Universo Marvel - Spider Man/Homem Aranha</h5>
+                            <p class="card-text">O Homem-Aranha, o alter ego de Peter Parker, é um super-herói das revistas em quadrinhos 
+                            publicadas pela Marvel Comics. Criado pelo escritor/editor Stan Lee e pelo escritor/artista Steve Ditko, 
+                            o Homem-Aranha surgiu em Amazing Fantasy #15, durante a Era de Prata dos Quadrinhos.
+                            A tatuagem de aranha pode ter vários significados, como:Ascensão na carreira; 
+                            Liderança e conselho; Poder de criatividade; Sacrifício contínuo.</p>
+                            <p class="card-number">A parir de R$ 349,99</p>
+                            <a href="contato.php" class="btn btn-primary add-to-cart"data-nome="Item 1" data-preco="10">Entre em contato <target="_blank"></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="card mb-4">
+                        <img src="img/7saias.png" class="card-img-top" alt="Sete Saias - Tatoo Religiosa">
+                        <div class="card-body">
+                            <h5 class="card-title">Sete Saias - Tatoo Religiosa</h5>
+                            <p class="card-text">A Pomba Gira Sete Saias é uma entidade mística que trabalha no mundo espiritual, 
+                            que aparece sempre com suas sete saias e colares que dão sete voltas. Uma tatuagem com o tema Sete Saias 
+                            pode ser uma homenagem espiritual e uma forma de transformar uma frase antiga em algo novo</p>
+                            <p class="card-number">A partir de R$ 199,99</p>
+                            <a href="contato.php" class="btn btn-primary add-to-cart"data-nome="Item 1" data-preco="10">Entre em contato <target="_blank"></a>
+                        </div>
+                    </div>
+                </div>
+                               
+                <!-- Repita o código acima para adicionar mais pratos -->
+            </div>
+        </div>
+    
+    <footer class="bg-warning text-white py-3 text-center">
+    <a href="#hero" class="btn btn-light">  Voltar à Home</a>
+       <font color="black"> <p ><br>&copy; 2024 Ric Londono. Todos os direitos reservados. Desenvolvido por Eliane Silva.</p> </font>
+       
+    </footer>
+    <!-- Adicione os links para os arquivos JavaScript do Bootstrap -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script>
+         $(document).ready(function() {
+            var carrinho = [];
+
+            $('.add-to-cart').click(function() {
+                var nome = $(this).data('nome');
+                var preco = $(this).data('preco');
+
+                carrinho.push({ nome: nome, preco: preco });
+
+            });
+
+            $('#enviar-whatsapp').click(function() {
+                var mensagem = "Pedido:\n";
+                var total = 0;
+
+                for (var i = 0; i < carrinho.length; i++) {
+                    var item = carrinho[i];
+                    mensagem += item.nome + " - R$" + item.preco.toFixed(2) + "\n";
+                    total += item.preco;
+                }
+
+                mensagem += "Total: R$" + total.toFixed(2);
+
+                var telefone = "5511985113254";
+                var mensagemWhatsApp = encodeURIComponent(mensagem);
+                var linkWhatsApp = "https://wa.me/" + telefone + "?text=" + mensagemWhatsApp;
+                window.open(linkWhatsApp);
+            });
+        });
+
+            // Função para atualizar o carrinho
+                function updateCart() {
+                const carrinhoContainer = document.getElementById("cart-items");
+                const cartTotalElement = document.getElementById("cart-total");
+                let cartTotal = 0;
+
+                // Limpa os itens do carrinho
+                carrinhoContainer.innerHTML = "";
+
+                // Adiciona os itens do carrinho ao HTML
+                carrinho.forEach(function(item) {
+                    const row = document.createElement("tr");
+                    const nome = document.createElement("td");
+                    const preco = document.createElement("td");
+                    const carrinho = Array.from(carrinhoContainer.getElementsByTagName("tr"))
+                .map(function(row) {
+                        const cells = Array.from(row.getElementsByTagName("td"));
+                        return cells.map(function(cell) {
+                        return cell.textContent;
+                        });
+                    });
+                const flattenedItems = carrinho.flat();
+                    const hiddenField = document.getElementById("itensTotal");
+                    hiddenField.value = flattenedItems.join(", ");
+                    nome.textContent = item.name;
+                    preco.textContent = "R$ " + item.price.toFixed(2);
+
+                    row.appendChild(nome);
+                    row.appendChild(preco);
+                    carrinhoContainer.appendChild(row);
+
+                    cartTotal += item.price;
+                    
+                    document.getElementById("itensTotal").value = carrinho;
+                    document.getElementById("valorTotal").value = cartTotal;
+
+                });
+
+                // Atualiza o total do carrinho
+                document.getElementById("btn-limpar").addEventListener("click", function() {
+                    carrinho.length = 0; // Limpa o array carrinho
+                    updateCart(); // Atualiza o carrinho para exibir os itens limpos
+                });
+
+                cartTotalElement.textContent = "R$ " + cartTotal.toFixed(2);
+            }
+
+            // Função para adicionar um item ao carrinho
+            function addToCart(nome, preco) {
+                const item = {
+                    name: nome,
+                    price: preco
+                };
+
+                carrinho.push(item);
+                updateCart();
+            }
+            // Adiciona o evento de clique aos botões "Adicionar ao Carrinho"
+            addToCartButtons.forEach(function(button) {
+                button.addEventListener("click", function(event) {
+                    const cardBody = event.target.parentNode;
+                    const nome = cardBody.querySelector(".card-title").textContent;
+                    const preco = parseFloat(cardBody.querySelector(".card-number").textContent.replace("R$ ", "").replace(",", "."));
+                    addToCart(nome, preco);
+                    event.preventDefault();
+                });
+            });
+        });
+
+        // Atualizar o campo escondido com os itens do carrinho
+    const hiddenField = document.getElementById("itensTotal");
+    const carrinho = Array.from(carrinhoContainer.getElementsByTagName("tr"))
+      .map(function(row) {
+        return row.cells[0].textContent;
+      });
+    hiddenField.value = JSON.stringify(carrinho);
+
+    // Atualizar o campo escondido com o valor total do carrinho
+    const total = Array.from(carrinhoContainer.getElementsByTagName("tr"))
+      .reduce(function(sum, row) {
+        return sum + parseFloat(row.cells[1].textContent.replace("R$ ", ""));
+      }, 0);
+    const totalField = document.getElementById("valorTotal");
+    totalField.value = total.toFixed(2);
+    </script>
+    <script>
+        const btnLimpar = document.getElementById("btn-limpar");
+        btnLimpar.addEventListener("click", function() {
+            const carrinhoContainer = document.getElementById("cart-items");
+            carrinhoContainer.innerHTML = ""; // Limpa o conteúdo do carrinho
+            carrinho.length = 0; // Remove todos os itens do carrinho
+            cartTotal = 0; // Zera o total do carrinho
+            cartTotalElement.textContent = "R$ 0,00"; // Atualiza a exibição do total do carrinho
+        });
+    </script>
+    
+
+</body>
+
+</html>
